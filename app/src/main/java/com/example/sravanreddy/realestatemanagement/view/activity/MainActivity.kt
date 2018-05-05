280fc5fa72b819256c563f02ce433db37821a6ba
srawan reddy <srawanreddy@gmail.com>
2018-05-04 23:47:47 -0500

    Kotlin Commit

---
+++ app/src/main/java/com/example/sravanreddy/realestatemanagement/view/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sravanreddy.realestatemanagement.view.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sravanreddy.realestatemanagement.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
